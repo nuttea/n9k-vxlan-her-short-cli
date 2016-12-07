8701b7c67cc9e36bfa234fab52d4fc84668d6e69
nuttea <nuttea@gmail.com>
2016-12-07 19:53:53 +0700

    test and working
    add vlan and vni mapping
    add vni member to vne1 with ingress replication

--- vxlan-her.py
+++ vxlan-her.py
@@ -1,26 +1,25 @@
-
 #!/usr/bin/env python
 #
 #
 # Copyright (C) 2013 Cisco Systems Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); 
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at 
+# You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0 
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and 
+# See the License for the specific language governing permissions and
 # limitations under the License.
 #
 #vlan 2001
 # vn-segment 22001
 # exit
 #int nve 1
-# memver vni 22001
+# member vni 22001
 #    ingress-replication protocol static
 #      peer-ip 2.2.2.2
 #end
@@ -30,9 +29,10 @@
 import re
 import sys
 from cli import *
-from xml.dom import minidom 
+from xml.dom import minidom
 
 
-cli('vlan '+ sys.argv[0] +';vn-segment '+ sys.argv[1] +' ;int nve 1 ;memver vni '+ sys.argv[1] +' ;ingress-replication protocol static ;peer-ip 2.2.2.2')
+cli('configure terminal ;vlan '+ sys.argv[1] +' ;vn-segment '+ sys.argv[2])
+cli('configure terminal ;interface nve1 ;member vni '+ sys.argv[2] +' ;ingress-replication protocol static ;peer-ip 2.2.2.2')
 
-cli('show nve vni ingress-replication '+ sys.argv[1])
+r = cli('show nve vni ingress-replication ' + sys.argv[2]); print r
